09b3e20d8af50f84fe2ddb9942508c848975c476
Judah Nagler <judahn@sonic.net>
2013-12-21 00:32:01 -0800

    added widget areas to footer

--- footer.php
+++ footer.php
@@ -15,58 +15,35 @@
 
 <footer id="footer" class="site-footer footer row" role="contentinfo">
 	<div class="container">
-		<div class="col-sm-4 footer-widget">
-			<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location' => 'primary',
-						'container_class' => '',
-						'menu_class' => 'nav navbar-nav list-unstyled',
-						'fallback_cb' => '',
-						'menu_id' => 'footer-menu',
-						'walker' => new wp_bootstrap_navwalker()
-					)
-				); ?>
-				<!-- <img id="avatar-wide" src="<?php echo get_template_directory_uri() . '/assets/img/avatar.jpg' ?>" alt=""> -->
+		<div class="footer-widgets row">
+			<div class="col-sm-4 no-pad">
+				<div class="widget">
+					<h3>Widget 1</h3>
+					<!-- The WordPress Menu goes here -->
+					<!--<?php wp_nav_menu(
+						array(
+							'theme_location' => 'primary',
+							'container_class' => '',
+							'menu_class' => 'nav navbar-nav list-unstyled',
+							'fallback_cb' => '',
+							'menu_id' => 'footer-menu',
+							'walker' => new wp_bootstrap_navwalker()
+						)
+					); ?>-->
+					<!-- <img id="avatar-wide" src="<?php echo get_template_directory_uri() . '/assets/img/avatar.jpg' ?>" alt=""> -->
+				</div>
 			</div>
-			<div class="col-sm-4 footer-widget">
-				<div class="container">
-					<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-					<script>
-					new TWTR.Widget({
-					  version: 2,
-					  type: 'profile',
-					  rpp: 4,
-					  interval: 6000,
-					  width: 216,
-					  height: 240,
-					  theme: {
-					    shell: {
-					      background: '#ffffff',
-					      color: '#292b2c'
-					    },
-					    tweets: {
-					      background: '#f6f6f6',
-					      color: '#292b2c',
-					      links: '#1388d2'
-					    }
-					  },
-					  features: {
-					    scrollbar: false,
-					    loop: false,
-					    live: true,
-					    hashtags: true,
-					    timestamp: true,
-					    avatars: false,
-					    behavior: 'all'
-					  }
-					}).render().setUser('@judahnagler').start();
-					</script>
+			<div class="col-sm-4 no-pad">
+				<div class="widget">
+					<h3>Widget 2</h3>
+				</div>
+			</div>
+			<div class="col-sm-4 no-pad">
+				<div class="widget">
+					<h3>Widget 3</h3>
 				</div>
 			</div>	
-			<div class="col-sm-4 footer-widget">
-				<div class="container">hi</div>
-			</div>	
+		</div>	
 	</div><!-- close .container -->
 </footer><!-- close #colophon -->
 
