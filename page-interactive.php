13e4ddde98200eead85efe3b18340adafad70e40
Judah Nagler <judahn@sonic.net>
2013-12-21 19:34:40 -0800

    MAJOR COMMIT: created thumbnail-posts.php to consolidate god from each page-[section].php. ALSO removed compass/reset scss and revised styles (still some to do) so that ul, ol can work in content sections by default.

--- page-interactive.php
+++ page-interactive.php
@@ -14,52 +14,20 @@
 
 <?php include 'section-opener.php' ?>
 
+	<?php
+		$args = array(
+			'post_type' => 'interactive',
+			'posts_per_page' => '-1',
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+		);
+		$the_query = new WP_Query( $args );
+	    $the_query->get_posts();
+	?>
 
+	<?php get_template_part( 'content', 'page' ); ?>
 
-
-<?php
-	$args = array(
-		'post_type' => 'interactive',
-		'posts_per_page' => '-1',
-		'orderby' => 'menu_order',
-		'order' => 'ASC',
-	);
-	$the_query = new WP_Query( $args );
-    $the_query->get_posts();
-?>
-
-<?php get_template_part( 'content', 'page' ); ?>
-
-<ul class="clearfix row row-thumbnails">
-
-	<!-- While posts exist, display them here -->
-	<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-		<!-- Linked Title -->
-
-		<li class="col-xs-6 col-sm-4 col-lg-3">
-			<a href="<?php the_permalink(); ?>" class="thumbnail">
-				<div class="thumb-content">
-					<?php if ( has_post_thumbnail() ) {
-						the_post_thumbnail(' img-responsive');
-					} else {?>
-						<img class="attachment- img-responsive wp-post-image" src="holder.js/300x200/industrial">
-					<?php } ?>
-					<div class="thumb-title"><h3><i class="fa fa-arrow-right"></i><?php the_title(); ?></h3></div>
-				</div>
-			</a>
-		</li>
-
-	<?php endwhile; else: ?>
-
-	<!-- If no posts, display this message -->
-		<p>There are no posts or pages here</p>
-
-	<?php endif; ?>
-</ul>
-
-
-
+	<?php include 'thumbnail-posts.php' ?>
 
 <?php include 'section-closer.php' ?>
 
