007fffdb43391a2f6af9cc4f046e49158c62998c
Judah Nagler <judahn@sonic.net>
2014-01-05 04:04:45 -0800

    thumb bax

--- above-header.php
+++ above-header.php
@@ -5,9 +5,9 @@
 <article class="row above-header hidden-xs">
 	<div id="above-header-container" class="container">
 		<?php if ( get_the_ID() == 163): ?>
-		<h1 class="pull-right" data-stellar-ratio="2">Blog</h1>
+		<h1 class="pull-right">Blog</h1>
 		<?php else: ?>
-		<h1 class="pull-right" data-stellar-ratio="2"><?php the_title(); ?></h1>
+		<h1 class="pull-right"><?php the_title(); ?></h1>
 		<?php endif; ?>
 	</div>
 </article>
