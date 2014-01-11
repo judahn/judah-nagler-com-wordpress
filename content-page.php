b5168bfc19b6fb3f1eb1776a91de548ea8eb3e82
Judah Nagler <judahn@sonic.net>
2014-01-11 04:48:55 -0800

    misc

--- content-page.php
+++ content-page.php
@@ -11,8 +11,9 @@
 		<h1 class="page-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
+	<?php if( the_content() != "" ): ?>
 	<div class="entry-content">
-		<?php the_content(); ?>
+		the_content();
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', '_tk' ),
@@ -20,5 +21,6 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+<?php endif; ?>
 	<?php edit_post_link( __( 'Edit', '_tk' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
