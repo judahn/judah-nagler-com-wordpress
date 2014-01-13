1ce65422cac00e73391406b19da267f6644be4f0
Judah Nagler <judahn@sonic.net>
2014-01-13 02:06:18 -0800

    moved cherry-blossoms and footer (& more) to their own scss files. made nav pos:fixed for single desktop views. added destaurate.svg file in prep for hopeful firefox css grayscale fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,24 @@
 /*
- *  CUSTOM JAVASCRIPT
+ *  CUSTOM.JS by Judah Nagler, 2014
  */
 
-$j=jQuery.noConflict();
 
-// Set active nav item for single posts based on URL
+// Set custom jQuery var
+
+$j = jQuery.noConflict();
+
+
+// On document ready:
+
 $j(document).ready(function() {
+
+
+	// Set active nav item for single posts based on URL
+
 	var pathname        = window.location.pathname;
 	var nav_about       = $j("#menu-item-130");
-	var nav_blog        = $j("#menu-item-97");
-	var nav_responsive  = $j("#menu-item-143");
+	var nav_blog        = $j("#menu-item-359");
+	var nav_responsive  = $j("#menu-item-360");
 	var nav_interactive = $j("#menu-item-160");
 	var nav_design      = $j("#menu-item-135");
 	var nav_contact  	= $j("#menu-item-145");
@@ -33,83 +42,132 @@ $j(document).ready(function() {
 		nav_contact.addClass("active");
 	}
 
+
+	// Set image container height and bottom padding on load
+
 	$j(".image").load(function(){
 		$j(this).height("auto").css({"padding-bottom":"0 !important"});
 	});
+
 	
-	// scroll opacity
+	// Set UI elements opacity based on scroll position
 
-    var currView    = 0;
-    var lastView    = -1;
+	var winW 		= $j(window).width();
+	var tablet      = 768;
+	var phone       = 480;
+	var currView    = 0;
+	var lastView    = -1;
 
-    var winW 		= $j(window).width();
-    var tablet      = 768;
-    var phone       = 480;
+
+	// On window scroll
 
 	function onWindowScroll() {
-        var scroll      = $j(window).scrollTop();
-        var isSingle	= $j("body").hasClass("single");
-        var offsetTop   = 350;
-        var minOp       = 0;
-        var maxOp       = 1;
-        var tgtOp		= (scroll / offsetTop);	
-        var navOp		= tgtOp * 4;		
-        var hdrOp 		= ((offsetTop * 0.618) - scroll) / 100;
+		var scroll      = $j(window).scrollTop();
+		var isSingle	= $j("body").hasClass("single");
+		var offsetTop   = 350;
+		var minOp       = 0;
+		var maxOp       = 1;
+		var tgtOp		= (scroll / offsetTop);	
+		var navOp		= tgtOp * 4;		
+		var hdrOp 		= ((offsetTop * 0.618) - scroll) / 100;
 
-        // Get window width
-        winW = $j(window).width();
+		// Get window width
 
-        // fade in main content
+		winW = $j(window).width();
 
-        if (!isSingle && winW >= tablet){
-        	if (tgtOp >= maxOp){
-	            tgtOp  = maxOp;
-	        }
-	        if (tgtOp <= minOp){
-	            tgtOp  = minOp;
-	        }
-	        if (navOp <= 0){
-	            navOp  = 0;
-	        }	
-	        if (currView != lastView) {
-	        	$j.stellar();
-	        }
-	        currView = 1;
-        } else {
-        	currView = 0;
-        	tgtOp = navOp = hdrOp = 1;
-        }
+		// fade in main content
 
-        $j(".main-content").css({"opacity":tgtOp});
-        $j(".above-header h1").css({"opacity":(hdrOp)});
+		if (!isSingle && winW >= tablet){
+			if (tgtOp >= maxOp){
+				tgtOp  = maxOp;
+			}
+			if (tgtOp <= minOp){
+				tgtOp  = minOp;
+			}
+			if (navOp <= 0){
+				navOp  = 0;
+			}	
+			if (currView != lastView) {
+				$j.stellar();
+			}
+			currView = 1;
+		} else {
+			currView = 0;
+			tgtOp = navOp = hdrOp = 1;
+		}
 
-        lastView = currView;
+		$j(".main-content").css({"opacity":tgtOp});
+		$j(".above-header h1").css({"opacity":(hdrOp)});
+
+		lastView = currView;
 
 
-        // set min top position for cherry blossoms on mobile scroll 
+		// set min top position for cherry blossoms on mobile scroll 
 
-        /*var cba     = $j("#cb-a");
-        var cbaMin  = -86;
-        var cbaTop 	= parseInt(cba.position().top);
+		/*var cba     = $j("#cb-a");
+		var cbaMin  = -86;
+		var cbaTop 	= parseInt(cba.position().top);
 
-        console.log("cbaTop: "+cbaTop, "cbaMin: "+cbaMin, cbaTop <= cbaMin);
+		console.log("cbaTop: "+cbaTop, "cbaMin: "+cbaMin, cbaTop <= cbaMin);
 
-        if (isSingle && winW <= tablet){
-        	
-        	if (cbaTop <= cbaMin){
-        		cba.css({ "top" : cbaMin + " !important" });
-        	}
-        }*/
-    }
+		if (isSingle && winW <= tablet){
+			
+			if (cbaTop <= cbaMin){
+				cba.css({ "top" : cbaMin + " !important" });
+			}
+		}*/
+	}
 
 	onWindowScroll();
+
+
+	// Responsive: Load images from directory
+
+	function loadImagesFromDir(){
+
+		var dir = "Src/themes/base/images";
+		var fileextension = ".jpg";
+		
+		$.ajax({
+
+			// This will retrieve the contents of the folder if the folder is configured as 'browsable'
+		
+			url: dir,
+			success: function (data) {
+	
+				// List all png file names in the page
+				$(data).find("a:contains(" + fileextension + ")").each(function () {
+	
+					var filename = this.href.replace(window.location.host, "").replace("http:///","");              
+	
+					$("body").append($("<img src=" + Dir + filename + "></img>"));
+	
+				});
+			}
+		});
+	}
+
+	
+
+	// Responsive: Slideshow
+
+	function responsiveSlideshow(){
+
+	}
+
+	// Bind window scroll function to 'scroll' and 'resize'
 	
 	$j(window).bind("scroll", onWindowScroll);
 	$j(window).bind("resize", onWindowScroll);
 
+
+	// Stellar.js
+
 	$j.stellar({
 		hideDistantElements: false
 	});
+
+
 
 
 	// auto scroll
@@ -119,11 +177,10 @@ $j(document).ready(function() {
 		i++;
 		$j(window).scrollTop(i);
 	}, 1);*/
-	
 
 	// intro animation
 
-	function introAnimation() {
+	/*function introAnimation() {
 		// fade in elements
-	}
+	}*/
 });
