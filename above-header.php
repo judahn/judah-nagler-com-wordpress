7d7a9c5d9f7896dd30225ffc5f99cf5eefcb2ec5
Judah Nagler <judahn@sonic.net>
2014-01-16 18:59:37 -0800

    set front page title manually in above-header.php

--- above-header.php
+++ above-header.php
@@ -6,6 +6,8 @@
 	<div id="above-header-container" class="container">
 		<?php if ( get_the_ID() == 163): ?>
 		<h1 class="pos-abs hidden-xs" data-stellar-ratio="0.42">Blog</h1>
+		<?php elseif( is_front_page()): ?>
+		<h1 class="pos-abs hidden-xs" data-stellar-ratio="0.42">Judah Nagler</h1>
 		<?php else: ?>
 		<h1 class="pos-abs hidden-xs"data-stellar-ratio="0.42"><?php the_title(); ?></h1>
 		<?php endif; ?>
