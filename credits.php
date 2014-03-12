1a3bff370a6ee60d29171b082ce34be8ee380928
Judah Nagler <judahn@sonic.net>
2014-03-12 15:39:58 -0700

    removed table from credits.. just put the copy and link in a 'p' tag. removed justified text from credits & description

--- credits.php
+++ credits.php
@@ -1,16 +1,8 @@
-    <div class="credits table-responsive no-border">
-      <table class="table">
+    <div class="credits">
 
         <?php if( get_field('client') ): ?>
-        <tr>
-          <td class="col-sm-4 col-md-4 no-pad-left">Developed for: </td>
-          <td>
-            <?php if( get_field('client_url') ): ?><a href="<?php the_field( 'client_url' ); ?>" target="_blank"><?php endif; ?> 
-               <?php the_field( 'client' ); ?>
-            <?php if( get_field('client_url') ): ?></a><?php endif; ?> 
-          </td>
-        </tr>
-        <?php endif; ?>
+        <p>Developed for: 
+          <?php if( get_field('client_url') ): ?><a href="<?php the_field( 'client_url' ); ?>" target="_blank"><?php endif; ?> <?php the_field( 'client' ); ?></a>
+        <?php endif; ?></p>
 
-      </table>
     </div>
